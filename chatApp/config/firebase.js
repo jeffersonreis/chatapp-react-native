b81dcb4ae7148b9cd13bd4935f89786e2a37183d
jeffersonreis <jeffersonluis.reis@gmail.com>
2022-02-27 22:00:49 -0300

    login e navigacao

--- chatApp/config/firebase.js
+++ chatApp/config/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, getauth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { Constants } from "expo-constants";
+import Constants from "expo-constants";
 
 // Firebase Config
 const firebaseConfig = {
-    apiKey: Constants.manifest.extra.apiKey,
-    authDomain: Constants.manifest.extra.authDomain,
-    projectId: Constants.manifest.extra.projectId,
-    storageBucket: Constants.manifest.extra.storageBucket,
-    messagingSenderId: Constants.manifest.extra.messagingSenderId,
-    appId: Constants.manifest.extra.appId,
-    databaseURL: Constants.manifest.extra.databaseURL,
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL,
 };
 
 // initialize Firebase
